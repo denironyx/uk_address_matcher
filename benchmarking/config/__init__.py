e11b96521bdc4e077c23dd17680720a677b5f69d
ThomasHepworth <thomas.hepworth@justice.gov.uk>
2026-03-19 17:30:02 +0000

    refactor: allow local loads for messy datasets

--- benchmarking/config/__init__.py
+++ benchmarking/config/__init__.py
@@ -4,12 +4,12 @@ from benchmarking.config.datasets import (
     load_dataset,
     maybe_enable_s3_for_path,
 )
-from benchmarking.config.sources import resolve_s3_path
+from benchmarking.config.sources import resolve_data_path
 
 __all__ = [
     "get_dataset_definition",
     "list_dataset_keys",
     "load_dataset",
     "maybe_enable_s3_for_path",
-    "resolve_s3_path",
+    "resolve_data_path",
 ]
